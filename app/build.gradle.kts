6bb9abe660c8f12cc1142409111f11e2b86afc14
erickimani05 <141523868+erickimani05@users.noreply.github.com>
2026-09-13 19:34:32 +0300

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ginera.tvremote"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ginera.tvremote"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
